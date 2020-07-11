87283517fd0d2a5f80d6a678c0fd1b0bfbad9b1d
ricardomurillo <ricardomurillo13@gmail.com>
2020-07-11 13:29:58 -0600

    docs agregado

---
+++ docs/precache-manifest.73674a8a0f9af23bf766917d872549f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7578027537a8062a0ecc0e7af92156f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ade7387004857a3b119",
+    "url": "/static/css/main.ebd89ac3.chunk.css"
+  },
+  {
+    "revision": "f6104528819a19072ec9",
+    "url": "/static/js/2.9e57f6ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9e57f6ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ade7387004857a3b119",
+    "url": "/static/js/main.f4278109.chunk.js"
+  },
+  {
+    "revision": "e775e7a58d8a9a5e5140",
+    "url": "/static/js/runtime-main.d76e4de2.js"
+  }
+]);
