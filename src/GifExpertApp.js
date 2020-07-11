cc48ce30226125e542c8966f58f35a36f24a208e
ricardomurillo <ricardomurillo13@gmail.com>
2020-07-11 13:28:56 -0600

    first commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
+import { GifGrid } from "./components/GifGrid";
+
+export const GifExpertApp = () => {
+  //const categories = ["One Punch Man", "Naruto", "Dragon Ball"];
+  const [categories, setCategories] = useState(["Game of Thrones"]);
+
+  return (
+    <>
+      <h2>GifExpertApp</h2>
+      <AddCategory setCategories={setCategories} />
+      <hr />
+
+      {categories.map(category => (
+        <GifGrid key={category} category={category} />
+      ))}
+    </>
+  );
+};
